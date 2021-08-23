7af829d513add0ee67c68a4bc301d39df49cf9f8
Julio Griebeler CH <julio.griebeler@ext.collectivehealth.com>
2021-08-23 16:57:57 -0300

    Adding temp page

---
+++ static/js/main.js
@@ -0,0 +1,47 @@
+
+
+const availableLangs = ["de", "pt", "en"]
+
+function loadLangButtons(){
+    console.log(1);
+    availableLangs.forEach(function(lang){
+        let aField = document.createElement('a');
+        let linkText = document.createTextNode(lang);
+        aField.appendChild(linkText);
+        aField.title = lang;
+        const absUrl = window.location.pathname;
+        aField.href = absUrl + "?lang=" + lang;
+        document.getElementById("langButtons").append(" ");
+        document.getElementById("langButtons").appendChild(aField);
+        document.getElementById("langButtons").append(" ");
+    });
+}
+
+function setLanguage() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const lang = urlParams.get('lang');
+    if (lang != undefined && availableLangs.includes(lang)) {
+        loadJSON(setLangData, lang);
+    }
+}
+
+function setLangData(langFileContent){
+    let jsonData = JSON.parse(langFileContent);
+    Object.keys(jsonData).forEach(function(key){
+        let text = jsonData[key];
+        document.getElementById(key).innerText = text;
+    });
+}
+
+function loadJSON(callback, fileName) {
+    let request = new XMLHttpRequest();
+    request.overrideMimeType("application/json");
+    request.open('GET', "static/lang/" + fileName + ".json", true);
+    request.onreadystatechange = function () {
+          if (request.readyState == 4 && request.status == "200") {
+            callback(request.responseText);
+          }
+    };
+    request.send(null);
+ }
